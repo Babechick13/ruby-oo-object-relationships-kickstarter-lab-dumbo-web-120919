c7555b71a4a4f8dd99758be3c1e19cfa92a4f0c4
Jessica Natovich <jessicanatovich@yahoo.com>
2019-12-17 18:49:09 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,16 +1,24 @@
-require "pry"
-
 class Project
-attr_reader :title 
+  attr_reader :title, :backers
+  attr_reader :title
 
-def initialize(title)
-  @title = title 
-  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    backers << backer
+    backer.backed_projects << self
+    ProjectBacker.new(self, backer)
+  end
+
+  def backers
+    project_backers = ProjectBacker.all.select do |pb| 
+      pb.project == self
+    end
+    project_backers.map do |pb|
+      pb.backer
+    end
+  end
 end
-
-def add_backer(backer)
-  ProjectBacker.new(self, backer)
-  #binding.pry
-end 
-
-end 
